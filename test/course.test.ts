d5c9ec93bc657d73f95500b5d6ccf3d0a6ef9aad
mtfs1 <math.santos.2002@gmail.com>
2022-01-06 15:45:42 -0300

    refactor(test): move module tests to their own file

--- test/course.test.ts
+++ test/course.test.ts
@@ -3,13 +3,6 @@ import Lecture from "../src/entities/lecture"
 import Course from "../src/entities/course"
 
 describe("Course", () => {
-    it("should be able to add lectures to modules", () => {
-        const module = new Module("Fundamentals")
-        const lecture =  new Lecture("Branching","https://youtube.com/1234")
-
-        module.add(lecture)
-        expect(module.includes(lecture)).toBeTruthy()
-    })
 
     it("should be able to add modules do courses", () => {
         const course = new Course("azure-devops", 
@@ -22,26 +15,4 @@ describe("Course", () => {
         expect(course.includes(module)).toBeTruthy()
     })
 
-    it("should not be able to add the same lecture twice in the same module", () => {
-        const module = new Module("Fundamentals")
-        const lecture = new Lecture("Branching","https://youtube.com/1234")
-        const sameLecture = new Lecture("Branching","https://youtube.com/1234")
-
-        module.add(lecture)
-        module.add(lecture)
-        module.add(sameLecture)
-        expect(module.includes(lecture)).toBeTruthy()
-        expect(module.numberOfLectures).toBe(1)
-    })
-
-    it("should not be able to add two lectures with same name in the same module", () => {
-        const module = new Module("Fundamentals")
-        const lecture = new Lecture("Branching","https://youtube.com/1234")
-        const otherLecture = new Lecture("Branching","https://youtube.com/4321")
-
-        module.add(lecture)
-        module.add(otherLecture)
-        expect(module.includes(lecture)).toBeTruthy()
-        expect(module.numberOfLectures).toBe(1)
-    })
 })
---
+++ test/module.test.ts
@@ -0,0 +1,37 @@
+import Module from "../src/entities/module"
+import Lecture from "../src/entities/lecture"
+
+describe("Module", () => {
+
+    it("should be able to add lectures to modules", () => {
+        const module = new Module("Fundamentals")
+        const lecture =  new Lecture("Branching","https://youtube.com/1234")
+
+        module.add(lecture)
+        expect(module.includes(lecture)).toBeTruthy()
+    })
+
+    it("should not be able to add the same lecture twice in the same module", () => {
+        const module = new Module("Fundamentals")
+        const lecture = new Lecture("Branching","https://youtube.com/1234")
+        const sameLecture = new Lecture("Branching","https://youtube.com/1234")
+
+        module.add(lecture)
+        module.add(lecture)
+        module.add(sameLecture)
+        expect(module.includes(lecture)).toBeTruthy()
+        expect(module.numberOfLectures).toBe(1)
+    })
+
+    it("should not be able to add two lectures with same name in the same module", () => {
+        const module = new Module("Fundamentals")
+        const lecture = new Lecture("Branching","https://youtube.com/1234")
+        const otherLecture = new Lecture("Branching","https://youtube.com/4321")
+
+        module.add(lecture)
+        module.add(otherLecture)
+        expect(module.includes(lecture)).toBeTruthy()
+        expect(module.numberOfLectures).toBe(1)
+    })
+    
+})
