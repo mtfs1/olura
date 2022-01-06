ae452ea72b95f6d2194e1ffa808626b27befb758
mtfs1 <math.santos.2002@gmail.com>
2022-01-06 14:48:35 -0300

    test(module): cannot add two lectures with same name
    The past test ensured that we cannot add the same lecture (i.e. with same
    attributes) twice in the same module. This test is different, it focuses
    only in the name of the lecture. Two lectures can have same name and still
    be treated as different because of the other attributes, so this test
    comes in.

--- test/course.test.ts
+++ test/course.test.ts
@@ -27,11 +27,20 @@ describe("Course", () => {
         const lecture = new Lecture("Branching","https://youtube.com/1234")
         const sameLecture = new Lecture("Branching","https://youtube.com/1234")
 
-        sameLecture.equals(lecture)
-
         module.add(lecture)
         module.add(lecture)
         module.add(sameLecture)
+        expect(module.includes(lecture)).toBeTruthy()
+        expect(module.numberOfLectures).toBe(1)
+    })
+
+    it("should not be able to add two lectures with same name in the same module", () => {
+        const module = new Module("Fundamentals")
+        const lecture = new Lecture("Branching","https://youtube.com/1234")
+        const otherLecture = new Lecture("Branching","https://youtube.com/4321")
+
+        module.add(lecture)
+        module.add(otherLecture)
         expect(module.includes(lecture)).toBeTruthy()
         expect(module.numberOfLectures).toBe(1)
     })
