fa4aef3b26b30ea31dcd515518e2bdc9b20642db
mtfs1 <math.santos.2002@gmail.com>
2022-01-11 21:00:10 -0300

    test(course): rearrange module order
    The test uses the unimplemented methods "move" and "position". The first
    will move the position of the module in the array of modules inside the
    course and the second will receive a module and will return its position
    inside the array of modules.
    
    The two methods receive a module and must search for another module
    inside the array that equals the first module. The positions start at
    one, unlike the real indexes of the array.

--- test/course.test.ts
+++ test/course.test.ts
@@ -15,4 +15,23 @@ describe("Course", () => {
         expect(course.includes(module)).toBeTruthy()
     })
 
+    it("should be able to rearrange modules order", () => {
+        const course = new Course("azure-devops", 
+            "Continuous Delivery and DevOps with Azure DevOps: Source Control with Git")
+
+        const module1 = new Module("Fundamentals")
+        course.add(module1)
+
+        const module2 = new Module("Couse Overview")
+        course.add(module2)
+
+        const module3 = new Module("Source Control With Git")
+        course.add(module3)
+
+       course.move(module2, 1)
+
+       expect(course.position(module2)).toBe(1)
+       expect(course.position(module1)).toBe(2)
+       expect(course.position(module3)).toBe(3)
+    })
 })
