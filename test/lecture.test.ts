0533cf7e21c48d3a7f37fbb6ba608d5a1c5f6584
mtfs1 <math.santos.2002@gmail.com>
2022-01-27 17:45:00 -0300

    test(lecture): further material
    A lecture should contain a link to the main video, but it may also provide
    further contents if it is the will of the teatcher. Then, the lecture
    class must implement an interface to your interior array of materials.
    
    The Material interface must be created, and a lecture should be able
    to add, remove and see if contains objects from classes that implements
    the Material interface. The test also uses a class that must implement
    the interface, the Pdf.

---
+++ test/lecture.test.ts
@@ -0,0 +1,23 @@
+import Lecture from "../src/entities/lecture"
+import Material from "../src/entities/material"
+import Pdf from "../src/entities/pdf"
+
+describe("Lecture", () => {
+
+    it("should be able to add further material to lectures", () => {
+        const lecture: Lecture = new Lecture("Branching", "https://youtube.com/branching")
+        const branchingPdf: Material = new Pdf("Branching", "https://storage/branching.pdf")
+        lecture.add(branchingPdf)
+
+        expect(lecture.includes(branchingPdf)).toBeTruthy()
+    })
+
+    it("should be able to remove further material from lectures", () => {
+        const lecture: Lecture = new Lecture("Branching", "https://youtube.com/branching")
+        const branchingPdf: Material = new Pdf("Branching", "https://storage/branching.pdf")
+        lecture.add(branchingPdf)
+        lecture.remove(branchingPdf)
+
+        expect(lecture.includes(branchingPdf)).not.toBeTruthy()
+    })
+})
